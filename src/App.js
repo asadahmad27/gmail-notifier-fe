3e89672294131723c2a798d69e59c3bc74333806
asadahmad27 <amasadmughal@gmail.com>
2024-07-10 21:00:52 +0500

    env fixes

--- src/App.js
+++ src/App.js
@@ -1,48 +1,14 @@
 // src/App.js
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
-import EmailList from "./components/emailList";
 import Login from "./components/login";
-import axios from "axios";
-import { GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
-// import { auth } from "./firebaseConfig";
-// import { useAuthState } from "react-firebase-hooks/auth";
-// import axios from "axios";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
-  // const [user] = useAuthState(auth);
-  // const [tokens, setTokens] = useState(null);
-  const [user, setUser] = useState();
-  // useEffect(() => {
-  //   console.log("inside se efeect", userData);
-  //   if (userData?.user?.user?.uid) {
-  //     const { user } = userData?.user;
-  //     axios
-  //       .post("http://localhost:5000/store-tokens", {
-  //         userId: user?.uid,
-  //         email: user?.email,
-  //         tokens: {
-  //           access_token: userData?.credential?.accessToken,
-  //           id_token: userData?.credential.idToken,
-  //           refresh_token: user?.stsTokenManager?.refreshToken, // Ensure this is captured
-  //         },
-  //       })
-  //       .then((response) => {
-  //         console.log("Tokens stored:", response.data);
-  //       })
-  //       .catch((error) => {
-  //         console.error("Error storing tokens:", error);
-  //       });
-  //   }
-  // }, [userData]);
-
-  // console.log(userData, "tokens");
-
   return (
-    <GoogleOAuthProvider clientId="226341879966-a1nf9tfijbfkjqrlmqpdephpjd5ilquh.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_OAUTH2_CLIENT_ID}>
       <div className="App">
-        hello
         <Login />
       </div>
     </GoogleOAuthProvider>
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,61 +1,23 @@
 import React, { useState } from "react";
-import { signInWithGoogle, logOut, auth } from "../firebaseConfig";
 import { useGoogleLogin } from "@react-oauth/google";
-// import { useAuthState } from "react-firebase-hooks/auth";
 
-const Login = ({ onLogin }) => {
-  //   const [user] = useAuthState(auth);
-  // const user = null;
+const Login = () => {
   const [user, setUser] = useState();
-  const handleLogin = () => {
-    // signInWithEmailAndPassword(auth, email, password)
-    //     .then((userCredential) => {
-    //         // Signed in
-    //         const user = userCredential.user;
-    //         navigate("/home")
-    //         console.log(user);
-    //     })
-    //     .catch((error) => {
-    //         const errorCode = error.code;
-    //         const errorMessage = error.message;
-    //         console.log(errorCode, errorMessage)
-    //     });
-    // signInWithGoogle()
-    //   .then((user) => {
-    //     console.log(user);
-    //     onLogin(user);
-    //   })
-    //   .catch((e) => console.log(e));
-  };
 
   const login = useGoogleLogin({
     onSuccess: async (codeResponse) => {
-      console.log(codeResponse);
       setUser(codeResponse);
       const { code } = codeResponse;
       makeAPICall(code);
-      // Send the authorization code to your backend
-      // const res = await fetch("http://localhost:5000/store-tokens", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({ code }),
-      // });
-
-      // const data = await res.json();
-      // console.log("Backend response:", data);
     },
     onError: (error) => console.log("Login Failed:", error),
     flow: "auth-code",
   });
 
-  const makeAPICall = async (
-    code = "4/0ATx3LY7Ixa-YzwVzao3cTaaLqGVZdjn-yt7QcGkM1x3qw5N1ghGu_h17CkAND6qNhBF57A"
-  ) => {
+  const makeAPICall = async (code) => {
     // Send the authorization code to your backend
     const res = await fetch(
-      "https://gmail-notifier-local-build-fkbxp5ur6q-uc.a.run.app/store-tokens",
+      `{process.env.REACT_APP_BACKEND_URL}/store-tokens`,
       {
         method: "POST",
         headers: {
@@ -68,20 +30,10 @@ const Login = ({ onLogin }) => {
     const data = await res.json();
     console.log("Backend response:", data);
   };
-  console.log(user);
 
   return (
     <div>
       <button onClick={() => login()}>Sign in with Google 🚀 </button>
-      {/* <button onClick={() => makeAPICall()}>Sign in with Google 🚀 </button> */}
-      {/* {user ? (
-        <>
-          <p>Welcome, {user.displayName}</p>
-          <button onClick={logOut}>Log Out</button>
-        </>
-      ) : (
-        <button onClick={handleLogin}>Sign In with Google</button>
-      )} */}
     </div>
   );
 };
